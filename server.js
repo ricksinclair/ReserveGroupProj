76306491ae10653be6fa2c8662c267dd13f63bd5
Ulric Todman <rick.sinclair.nyc@gmail.com>
2018-12-22 12:58:10 -0500

    fschanges

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var tables = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/reserve", function(req, res) {
